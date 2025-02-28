b769cd3284d9a8f4fe5225145b62aee378e9175e
Sagar Niroula <sagarniroula112@gmail.com>
2025-02-28 11:55:13 +0545

    Few advance tasks completed.

--- dsa_a_100_days/src/com/ooptasks/advancedtasks/task1.java
+++ dsa_a_100_days/src/com/ooptasks/advancedtasks/task1.java
@@ -7,6 +7,40 @@ Implement the interface in CreditCard, DebitCard, and PayPal classes.
 Use polymorphism to process payments through different methods.
  */
 
+interface PaymentMethod {
+    void processPayment();
+}
+
+class CreditCard implements PaymentMethod {
+    @Override
+    public void processPayment() {
+        System.out.println("Payment processed for credit card.");
+    }
+}
+
+class DebitCard implements PaymentMethod {
+    @Override
+    public void processPayment() {
+        System.out.println("Payment processed for debit card.");
+    }
+}
+
+class PayPal implements PaymentMethod {
+    @Override
+    public void processPayment() {
+        System.out.println("Payment processed for paypal.");
+    }
+}
+
 public class task1 {
-    
+    public static void main(String[] args) {
+        PaymentMethod cc = new CreditCard();
+        cc.processPayment();
+
+        PaymentMethod dc = new DebitCard();
+        dc.processPayment();
+
+        PaymentMethod pp = new PayPal();
+        pp.processPayment();
+    }
 }
---
+++ dsa_a_100_days/src/com/ooptasks/advancedtasks/task2.java
@@ -0,0 +1,12 @@
+package com.ooptasks.advancedtasks;
+
+/*
+ * 10. Access Modifiers and Encapsulation:
+Task: Create a class BankAccount with private attributes balance and accountNumber. 
+Provide public getter and setter methods. 
+Implement the withdrawal method where the balance cannot go below a certain limit.
+ */
+
+public class task2 {
+    
+}
