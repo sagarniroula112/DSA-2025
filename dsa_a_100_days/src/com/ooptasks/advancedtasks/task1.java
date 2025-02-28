24b3803604039465ade20ed791cb83367986b26a
Sagar Niroula <sagarniroula112@gmail.com>
2025-02-28 11:49:32 +0545

    Starting advance tasks.

---
+++ dsa_a_100_days/src/com/ooptasks/advancedtasks/task1.java
@@ -0,0 +1,12 @@
+package com.ooptasks.advancedtasks;
+
+/*
+ * 9. Polymorphism with Interface:
+Task: Create an interface PaymentMethod with a method processPayment(). 
+Implement the interface in CreditCard, DebitCard, and PayPal classes. 
+Use polymorphism to process payments through different methods.
+ */
+
+public class task1 {
+    
+}
