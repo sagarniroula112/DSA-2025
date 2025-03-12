cb33851b0150dfb0396a43c19a734e8ab4e010f0
Sagar Niroula <sagarniroula112@gmail.com>
2025-03-12 17:25:42 +0545

    Multithreading completed.

---
+++ dsa_a_100_days/src/com/multithreadingtasks/Counterusingsynchronizeblock.java
@@ -0,0 +1,68 @@
+package com.multithreadingtasks;
+
+class Counter {
+    private int count;
+    
+    public void increment() {
+        synchronized(this) {
+            count++;
+            System.out.println("Count: " + count);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted!");
+            }
+        }
+    }
+    
+    public int getCount() {
+        return count;
+    }
+}
+
+class MyThread implements Runnable {
+    Thread t;
+    Counter c;
+    String name;
+    
+    MyThread(Counter counter, String tName) {
+        this.c = counter;
+        t = new Thread(this, tName);
+        this.name = tName;
+    }
+    
+    @Override
+    public void run() {
+        try{
+            for(int i=0;i<5;i++){
+                c.increment(); 
+                // Thread.sleep(2000);
+            }
+        }catch(Exception e) {
+            System.out.println("Exception occured.");
+        }
+    }
+}
+
+public class Counterusingsynchronizeblock {
+    public static void main(String[] args) {
+        Counter c = new Counter();
+        MyThread t1 = new MyThread(c, "First");
+        MyThread t2 = new MyThread(c, "Second");
+        MyThread t3 = new MyThread(c, "Third");
+        
+        t1.t.start();
+        t2.t.start();
+        t3.t.start();
+        
+        try{
+            t1.t.join();
+            t2.t.join();
+            t3.t.join();
+        }catch(InterruptedException e) {
+            System.out.println("Interrupt occured!");
+        }
+        
+        System.out.println("Final Count: " + c.getCount());
+    }
+}
