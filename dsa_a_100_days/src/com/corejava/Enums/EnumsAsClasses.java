75316a4273db24ee633a7c0a4fb1cbd8c26083a9
Sagar Niroula <sagarniroula112@gmail.com>
2025-03-12 18:37:42 +0545

    Using enum constructor, instance variable and methods.

--- dsa_a_100_days/src/com/corejava/Enums/EnumsAsClasses.java
+++ dsa_a_100_days/src/com/corejava/Enums/EnumsAsClasses.java
@@ -1,7 +1,36 @@
 package com.corejava.Enums;
 
+// Use of enum constructor, instance variable and method.
 
+enum Friends {
+    Rakesh(10, "Dhangadhi"), Prajwal(20, "Dhapakhel"), Samrat(5, "Pokhara"), Nirajan(12, "Butwal");
+    private int price;
+    private String location;
+
+    Friends(int p, String l) {
+        price = p;
+        location = l;
+    }
+
+    String getLocation() {
+        return location;
+    }
+
+    int getPrice() {
+        return price;
+    }
+}
 
 public class EnumsAsClasses {
-    
+    public static void main(String[] args) {
+        Friends fr;
+
+        System.out.println("Rakesh costs: " + Friends.Rakesh.getPrice());
+
+        Friends[] allFriends = Friends.values();
+        for(Friends f: allFriends) {
+            System.out.println(f.getPrice());
+            System.out.println(f.getLocation());
+        }
+    }
 }
