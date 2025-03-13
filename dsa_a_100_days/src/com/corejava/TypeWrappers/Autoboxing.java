48a52e7f832ec75ea218072a6810db901159463d
Sagar Niroula <sagarniroula112@gmail.com>
2025-03-13 11:13:03 +0545

    Boolean wrapper and intro to autoboxing.

---
+++ dsa_a_100_days/src/com/corejava/TypeWrappers/Autoboxing.java
@@ -0,0 +1,10 @@
+package com.corejava.TypeWrappers;
+
+public class Autoboxing {
+    public static void main(String[] args) {
+        Integer ino = 100; // autoboxing
+        int i = ino; // auto-unboxing
+
+        System.out.println(i + " " + ino);
+    }
+}
--- dsa_a_100_days/src/com/corejava/TypeWrappers/BooleanWrapper.java
+++ dsa_a_100_days/src/com/corejava/TypeWrappers/BooleanWrapper.java
@@ -2,6 +2,20 @@ package com.corejava.TypeWrappers;
 
 public class BooleanWrapper {
     public static void main(String[] args) {
-        
+        Boolean setState = Boolean.valueOf(true);
+        System.out.println(setState);
+
+        Boolean state2 = Boolean.valueOf("true");
+        System.out.println(state2);
+
+        Boolean state3 = Boolean.valueOf("FAlse");
+        System.out.println(state3);
+
+        // boolean state4 = "FAlse"; // WRONG
+        boolean state4 = false;
+        System.out.println(state4);
+
+        System.out.println(state2.booleanValue());
+        // System.out.println(state4.booleanValue()); // can't be called from primitive type boolean
     }
 }
