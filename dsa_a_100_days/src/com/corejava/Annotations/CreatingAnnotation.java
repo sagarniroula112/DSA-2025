e8f9ad0e7b4982f58a6cb70ddd1b97955833f54a
Sagar Niroula <sagarniroula112@gmail.com>
2025-03-15 08:50:20 +0545

    Annotation intro and Retention policy.

---
+++ dsa_a_100_days/src/com/corejava/Annotations/CreatingAnnotation.java
@@ -0,0 +1,14 @@
+package com.corejava.Annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyAnno {
+    String str();
+    int val();
+}
+
+public class CreatingAnnotation {
+    
+}
