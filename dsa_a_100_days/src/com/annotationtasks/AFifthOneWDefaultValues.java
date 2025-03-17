5b701c896f8913ab25de9b82ce359c25a7e70528
Sagar Niroula <sagarniroula112@gmail.com>
2025-03-17 10:15:35 +0545

    Testing default values in annotation interface.

--- dsa_a_100_days/src/com/annotationtasks/AFifthOneWDefaultValues.java
+++ dsa_a_100_days/src/com/annotationtasks/AFifthOneWDefaultValues.java
@@ -1,5 +1,32 @@
 package com.annotationtasks;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.Method;
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyAnno3 {
+    String str() default "Testing 123";
+    int val() default 123;
+}
+
 public class AFifthOneWDefaultValues {
     
+    @MyAnno3()
+    public static void myMethod() {
+        AFifthOneWDefaultValues afowdv = new AFifthOneWDefaultValues();
+        try {
+            Class<?> c = afowdv.getClass();
+            Method m = c.getMethod("myMethod");
+            MyAnno3 anno3 = m.getAnnotation(MyAnno3.class);
+
+            System.out.println(anno3);
+        } catch(NoSuchMethodException e) {
+            System.out.println("Exception occured.");
+        }
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
 }
