cb4b7b2311206c18aca4a6c7e83a9fb206f73888
Sagar Niroula <sagarniroula112@gmail.com>
2025-03-18 14:16:34 +0545

    Using write() to write on console.

---
+++ dsa_a_100_days/src/com/corejava/FileIO/WritingConsoleOp.java
@@ -0,0 +1,13 @@
+package com.corejava.FileIO;
+
+public class WritingConsoleOp {
+    public static void main(String[] args) {
+        int b;
+
+        b = 'A';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
+
+// Not used that often
