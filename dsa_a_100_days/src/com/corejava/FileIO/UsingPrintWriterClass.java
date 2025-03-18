2fb1a9355d9a7c3aabbbee069252a57ea01cc412
Sagar Niroula <sagarniroula112@gmail.com>
2025-03-18 14:48:15 +0545

    Using PrintWriter.

---
+++ dsa_a_100_days/src/com/corejava/FileIO/UsingPrintWriterClass.java
@@ -0,0 +1,13 @@
+package com.corejava.FileIO;
+
+import java.io.PrintWriter;
+
+public class UsingPrintWriterClass {
+    public static void main(String[] args) {
+        PrintWriter pw = new PrintWriter(System.out, true);
+
+        pw.println("This is a string.");
+        double d = 4.5e-7;
+        pw.println(d);
+    }
+}
