3de6e5f4086798a68690aa1d2896537b3299f5ea
Sagar Niroula <sagarniroula112@gmail.com>
2025-04-23 08:20:10 +0545

    LinkedList: Append at the end.

--- dsa_a_100_days/src/com/DSA/LinkedList/LinkedList.java
+++ dsa_a_100_days/src/com/DSA/LinkedList/LinkedList.java
@@ -21,7 +21,37 @@ public class LinkedList {
         length = 1;
     }
 
-    // public void append(int value) {}
+    public void printList() {
+        Node temp = head;
+        while(temp!=null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if(length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length = length + 1;
+    }
 
     // public void prepend(int value) {}
 
--- dsa_a_100_days/src/com/DSA/LinkedList/Main.java
+++ dsa_a_100_days/src/com/DSA/LinkedList/Main.java
@@ -2,8 +2,10 @@ package com.DSA.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(4);
+        LinkedList myLinkedList = new LinkedList(9);
 
-        
+        myLinkedList.append(6);
+
+        myLinkedList.printList();
     }
 }
